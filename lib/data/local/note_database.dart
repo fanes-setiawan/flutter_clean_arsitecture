812d88ba3cd6b7e44af0324ca2e35d8941d25ad9
fanes-setiawan <fanessetiawan.1401@gmail.com>
2025-07-30 14:18:56 +0700

    sqflite unfinis

---
+++ lib/data/local/note_database.dart
@@ -0,0 +1,56 @@
+// lib/data/local/note_database.dart
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:testapp/data/models/note_model.dart';
+
+class NoteDatabase {
+  static final NoteDatabase instance = NoteDatabase._init();
+
+  static Database? _database;
+
+  NoteDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('notes.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<List<NoteModel>> getAllNotes() async {
+    final db = await instance.database;
+    final result = await db.query('notes', orderBy: 'id DESC');
+    return result.map((map) => NoteModel.fromMap(map)).toList();
+  }
+
+  Future<int> insertNote(NoteModel note) async {
+    final db = await instance.database;
+    return await db.insert('notes', note.toMap());
+  }
+
+  Future<int> deleteNote(int id) async {
+    final db = await instance.database;
+    return await db.delete('notes', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
